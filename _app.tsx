502e9519b8c205cf03304f9a872a0e9203a663e6
S.watana <75829584+opepelab@users.noreply.github.com>
2021-11-05 16:27:12 +0900

    Update _app.tsx

--- _app.tsx
+++ _app.tsx
@@ -1,12 +1,24 @@
-import {AppProps} from 'next/app'
-import Layout from '../components/layout'
-import '../styles/globals.css'
+import { AppProps } from 'next/app'
+import { AnimatePresence } from "framer-motion";
+import '../styles/globals.scss'
+import '../styles/mobile.scss'
+import '../styles/img.scss'
+import Head from 'next/head'
 
-const MyApp: React.FC<AppProps> = ({ Component, pageProps }): JSX.Element => {
+
+const MyApp = ({ Component, pageProps, router }: AppProps): JSX.Element => {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="robots" content="noindex,nofollow" />
+      </Head>
+      <AnimatePresence exitBeforeEnter>
+        <Component {...pageProps} key={router.route} />
+      </AnimatePresence>
+    </>
   );
 }
+
 export default MyApp;
+
