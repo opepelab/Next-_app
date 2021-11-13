b397ba4c4e035808a9b262913746f8d727770659
S.watana <75829584+opepelab@users.noreply.github.com>
2021-11-13 13:10:06 +0900

    Update _app.tsx

--- _app.tsx
+++ _app.tsx
@@ -4,21 +4,30 @@ import '../styles/globals.scss'
 import '../styles/mobile.scss'
 import '../styles/img.scss'
 import Head from 'next/head'
-
+import { ThemeContext, ThemeProvider } from "styled-components";
+import { LightTheme, DarkTheme } from '../components/Theme/Theme'
+import { useState } from 'react'
+import GlobalStyles from '../components/Theme/Globalstyles'
 
 const MyApp = ({ Component, pageProps, router }: AppProps): JSX.Element => {
+  const [theme, toggleTheme] = useState('')
   return (
     <>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta name="robots" content="noindex,nofollow" />
       </Head>
-      <AnimatePresence exitBeforeEnter>
-        <Component {...pageProps} key={router.route} />
-      </AnimatePresence>
+      
+      <ThemeContext.Provider value={{theme, toggleTheme}}>
+        <ThemeProvider theme={theme === 'light' ? DarkTheme : LightTheme }>
+          <GlobalStyles />
+          <AnimatePresence exitBeforeEnter>
+            <Component {...pageProps} key={router.asPath} />
+          </AnimatePresence>
+        </ThemeProvider>
+      </ThemeContext.Provider>
     </>
   );
 }
 
 export default MyApp;
-
